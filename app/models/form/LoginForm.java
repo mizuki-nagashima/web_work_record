b63820d5bccdc020d6f8d292efa41ac1ee5ddf01
Mizuki Nagashima <mizuki-nagashima@kyms.co.jp>
2018-08-24 16:57:14 +0900

    社員番号メッセージ修正　LoginForm.java

--- app/models/form/LoginForm.java
+++ app/models/form/LoginForm.java
@@ -10,7 +10,7 @@ import play.data.validation.Constraints;
  * ログイン用フォーム
  */
 public class LoginForm {
-    @Constraints.Required(message = "社員番号入力してください。")
+    @Constraints.Required(message = "社員番号を入力してください。")
     @Constraints.Pattern(value = "^[0-9]+$", message = "社員番号は半角数字のみで入力してください。")
     public String employeeNo;
     @Constraints.Required(message = "パスワードを入力してください。")
