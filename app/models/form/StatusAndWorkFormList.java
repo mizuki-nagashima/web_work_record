2778d665cb772a63aaaeddb2b1d93479521e62c6
長島 聖月 <nagashima-mizuki@kms.local>
2018-09-13 15:11:50 +0900

    ステータスリスト用フォーム作成等

---
+++ app/models/form/StatusAndWorkFormList.java
@@ -0,0 +1,16 @@
+package models.form;
+
+import java.util.List;
+
+/**
+ * Created by mizuki-nagashima on 2018/09/13.
+ */
+
+/**
+ * ステータス表示用のフォームのリスト
+ */
+public class StatusAndWorkFormList {
+    public List<StatusAndWorkForm> statusAndWorkFormList;
+    //public List<StatusAndWorkForm> statusAndWorkFormList;
+
+}
