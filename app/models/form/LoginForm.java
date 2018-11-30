58d5baedd9c49ef265e0551147d8f830e506e9da
長島 聖月 <nagashima-mizuki@kms.local>
2018-11-30 17:38:20 +0900

    placeholderの作成・バリデーションの追加

--- app/models/form/LoginForm.java
+++ app/models/form/LoginForm.java
@@ -14,6 +14,8 @@ public class LoginForm {
     @Constraints.Pattern(value = "^[0-9]+$", message = "社員番号は半角数字のみで入力してください。")
     public String employeeNo;
     @Constraints.Required(message = "パスワードを入力してください。")
+    @Constraints.MinLength(value = 6, message = "6文字以上入力してください。")
+    @Constraints.MaxLength(value = 16, message = "16文字以下で入力してください。")
     @Constraints.Pattern(value = "^[a-zA-Z0-9]+$", message = "パスワードは半角英数字のみで入力してください。")
     public String password;
     public Integer loginNgCount =0;
