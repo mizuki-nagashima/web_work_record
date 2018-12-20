e133ab348af09596864a586e957f02f533fc2c47
長島 聖月 <nagashima-mizuki@kms.local>
2018-12-20 17:37:08 +0900

    form名前部分の正規表現修正

--- app/models/form/RegistEmpForm.java
+++ app/models/form/RegistEmpForm.java
@@ -16,7 +16,7 @@ public class RegistEmpForm {
     @Constraints.Pattern(value = "^[0-9]+$", message = "社員番号は半角数字のみで入力してください。")
     public String employeeNo;
     @Constraints.Required(message = "社員名を入力してください。")
-    @Constraints.Pattern(value = "^[^-~｡-ﾟ ]*$", message = "社員名は全角のみで入力してください。")
+    @Constraints.Pattern(value = "^[^ -~｡-ﾟ]*$", message = "社員名は全角のみで入力してください。")
     public String employeeName;
     @Constraints.Required(message = "社員名カナを入力してください。")
     @Constraints.Pattern(value = "^[ァ-ヶー]*$", message = "社員名カナは全角カナのみで入力してください。")
